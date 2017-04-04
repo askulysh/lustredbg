2f917d5130b4c42cf0578da5611e29cfab391a48
Andriy Skulysh <askulysh@gmail.com>
2017-04-04 22:15:22 +0300

    add kiblnd.py
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

---
+++ kiblnd.py
@@ -0,0 +1,53 @@
+# kiblnd functions
+
+from __future__ import print_function
+
+from pykdump.API import *
+
+max_req = 10
+
+def print_nid(nid) :
+    print("%d.%d.%d.%d" % ((nid >>24) & 0xff, (nid >> 16) & 0xff,
+                            (nid >> 8) & 0xff, nid & 0xff))
+
+def show_peers() :
+    kiblnd_data = readSymbol("kiblnd_data")
+    for i in xrange(0, kiblnd_data.kib_peer_hash_size) :
+        peers = readSUListFromHead(kiblnd_data.kib_peers[i],
+                "ibp_list", "struct kib_peer")
+        if peers :
+            print("i = %d" % i)
+            for peer in peers :
+                print(peer)
+                print_nid(peer.ibp_nid)
+
+def find_tx(req) :
+    kiblnd_data = readSymbol("kiblnd_data")
+    for i in xrange(0, kiblnd_data.kib_peer_hash_size) :
+        peers = readSUListFromHead(kiblnd_data.kib_peers[i],
+                "ibp_list", "struct kib_peer")
+        if peers :
+            for peer in peers :
+                print_nid(peer.ibp_nid)
+                txs = readSUListFromHead(peer.ibp_tx_queue,
+                "tx_list", "struct kib_tx")
+                for tx in txs :
+                    md = tx.tx_lntmsg[0].msg_md
+                    if md.md_iov.iov[0].iov_base == req.rq_reqbuf :
+                        print(tx)
+                        return
+
+if ( __name__ == '__main__'):
+    import argparse
+
+    parser =  argparse.ArgumentParser()
+    parser.add_argument("-r","--request", dest="req", default = 0)
+    parser.add_argument("-n","--num", dest="n", default = 0)
+    args = parser.parse_args()
+    if args.n != 0 :
+        max_req = n
+    if args.req != 0 :
+        req = readSU("struct ptlrpc_request", int(args.req, 0))
+        find_tx(req)
+    else :
+        show_peers()
