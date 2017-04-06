9057ab48c4dbd1cb8342f00d23fd13de6701a4c6
Andriy Skulysh <askulysh@gmail.com>
2017-04-06 14:09:22 +0300

    lnet: add parsing of gni nid
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- lnet.py
+++ lnet.py
@@ -5,8 +5,13 @@ from __future__ import print_function
 from pykdump.API import *
 
 def nid2str(nid) :
-    return ("%d.%d.%d.%d" % ((nid >>24) & 0xff, (nid >> 16) & 0xff,
-                            (nid >> 8) & 0xff, nid & 0xff))
+    lnd = nid >> 48
+    if lnd == 5 :
+        return ("%d.%d.%d.%d" % ((nid >>24) & 0xff, (nid >> 16) & 0xff,
+                                (nid >> 8) & 0xff, nid & 0xff))
+    elif lnd == 13 :
+        return ("%d@gni" % (nid & 0xffffffff))
+
 def print_nid(nid) :
     print(nid2str(nid))
 
