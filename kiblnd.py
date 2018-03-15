8bb5727a80631a8c91bc8cefaf78d2d23aa1f73b
Andriy Skulysh <askulysh@gmail.com>
2018-03-15 13:20:30 +0200

    kiblnd: look for tx in connd queues
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- kiblnd.py
+++ kiblnd.py
@@ -43,13 +43,37 @@ def find_tx_in_list(handle, txs) :
                 return tx
     return 0
 
-def find_tx_in_conn(handle, member) :
+def find_tx_in_conn_queue(handle, conn, member) :
     txs = readSUListFromHead("conn." + member,
             "tx_list", "struct kib_tx")
     tx = find_tx_in_list(handle, txs)
     if tx != 0 :
         print("found ", tx, " in ", member)
     return tx
+
+def find_tx_in_conn(handle, conn) :
+    tx = find_tx_in_conn_queue(handle, conn, "ibc_tx_noops")
+    if tx != 0 :
+        return tx
+    tx = find_tx_in_conn_queue(handle, conn, "ibc_tx_queue_nocred")
+    if tx != 0 :
+        return tx
+    tx = find_tx_in_conn_queue(handle, conn, "ibc_tx_queue_rsrvd")
+    if tx != 0 :
+        return tx
+    tx = find_tx_in_conn_queue(handle, conn, "ibc_tx_queue")
+    if tx != 0 :
+        return tx
+    tx = find_tx_in_conn_queue(handle, conn, "ibc_active_txs")
+    if tx != 0 :
+        return tx
+
+def find_tx_in_conn_list(handle, conns) :
+    for conn in conns :
+        tx = find_tx_in_conn(handle, conn)
+        if tx != 0 :
+            return tx
+    return 0
 
 def find_tx_by_handle(handle) :
     kiblnd_data = readSymbol("kiblnd_data")
@@ -63,26 +87,27 @@ def find_tx_by_handle(handle) :
                     "tx_list", "struct kib_tx")
                 tx = find_tx_in_list(handle, txs)
                 if tx != 0 :
-                    print("found ", tx, " in ", ibp_tx_queue)
+                    print("found ", tx, " in ibp_tx_queue")
                     return tx
                 conns = readSUListFromHead(peer.ibp_conns,
                     "ibc_list", "struct kib_conn")
-                for conn in conns :
-                    tx = find_tx_in_conn(handle, "ibc_tx_noops")
-                    if tx != 0 :
-                        return tx
-                    tx = find_tx_in_conn(handle, "ibc_tx_queue_nocred")
-                    if tx != 0 :
-                        return tx
-                    tx = find_tx_in_conn(handle, "ibc_tx_queue_rsrvd")
-                    if tx != 0 :
-                        return tx
-                    tx = find_tx_in_conn(handle, "ibc_tx_queue")
-                    if tx != 0 :
-                        return tx
-                    tx = find_tx_in_conn(handle, "ibc_active_txs")
-                    if tx != 0 :
-                        return tx
+                tx = find_tx_in_conn_list(handle, conns)
+                if tx != 0 :
+                    return tx
+
+    print("kib_connd_zombies :")
+    conns = readSUListFromHead(kiblnd_data.kib_connd_zombies,
+            "ibc_list", "struct kib_conn")
+    tx = find_tx_in_conn_list(handle, conns)
+    if tx != 0 :
+        return tx
+    print("kib_connd_conns :")
+    conns = readSUListFromHead(kiblnd_data.kib_connd_conns,
+            "ibc_list", "struct kib_conn")
+    tx = find_tx_in_conn_list(handle, conns)
+    if tx != 0 :
+        return tx
+
     return 0
 
 if ( __name__ == '__main__'):
