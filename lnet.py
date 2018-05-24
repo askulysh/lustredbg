8c2373e1f2ad590fbebdaed2c6dcda3cbda8a434
Andriy Skulysh <askulysh@gmail.com>
2018-05-24 11:11:52 +0300

    lnet: parse socklnd nid
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- lnet.py
+++ lnet.py
@@ -6,7 +6,10 @@ from pykdump.API import *
 
 def nid2str(nid) :
     lnd = nid >> 48
-    if lnd == 5 :
+    if lnd == 2 :
+        return ("%d.%d.%d.%d" % ((nid >>24) & 0xff, (nid >> 16) & 0xff,
+                                (nid >> 8) & 0xff, nid & 0xff))
+    elif lnd == 5 :
         return ("%d.%d.%d.%d" % ((nid >>24) & 0xff, (nid >> 16) & 0xff,
                                 (nid >> 8) & 0xff, nid & 0xff))
     elif lnd == 9 :
