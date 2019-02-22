c78b34ef8c5574f161d1522e4e31bd90af851247
Andriy Skulysh <askulysh@gmail.com>
2019-02-22 12:09:48 +0200

    lnet: add --nid to parse nid
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- lnet.py
+++ lnet.py
@@ -20,3 +20,12 @@ def nid2str(nid) :
 def print_nid(nid) :
     print(nid2str(nid))
 
+if ( __name__ == '__main__'):
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n","--nid", dest="nid", default = 0)
+    args = parser.parse_args()
+    if args.nid != 0 :
+        print_nid(int(args.nid, 16))
+
