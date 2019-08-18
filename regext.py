bdc163e72935cc16634dbf2f3e792d75901791b8
Andriy Skulysh <askulysh@gmail.com>
2019-08-18 21:31:05 +0300

    register commands on startup add pycrashext.
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

---
+++ regext.py
@@ -0,0 +1,44 @@
+from crash import register_epython_prog as rprog
+
+from pykdump.API import *
+
+import os
+import sys
+
+help = '''
+Show device related information
+'''
+
+rprog("ptlrpc", "ptlrpc requst information",
+      "-h   - list available options",
+      help)
+
+help = '''
+Show device related information
+'''
+
+rprog("ldlm_lock", "Device information",
+      "-h   - list available options",
+      help)
+
+
+rprog("obd", "Device information",
+      "-h   - list available options",
+      help)
+
+rprog("mdt", "MDT device information",
+      "-h   - list available options",
+      help)
+
+rprog("osd", "OSD device information",
+      "-h   - list available options",
+      help)
+
+rprog("cl_lock", "cl_lock information",
+      "-h   - list available options",
+      help)
+
+rprog("cl_io", "cl_io information",
+      "-h   - list available options",
+      help)
+
