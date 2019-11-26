57653615506fea8cfc91f88f2b1db21174add5d7
Andriy Skulysh <askulysh@gmail.com>
2019-11-26 11:51:52 +0200

    ptlrpc: show MDS_HSM_PROGRESS requests
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- ptlrpc.py
+++ ptlrpc.py
@@ -551,6 +551,8 @@ def show_ptlrpc_request_buf(req) :
         show_request_fmt(req, "RQF_MDS_GETXATTR")
     elif body.pb_opc == opcodes.MDS_CLOSE :
         show_request_fmt(req, "RQF_MDS_CLOSE")
+    elif body.pb_opc == opcodes.MDS_HSM_PROGRESS :
+        show_request_fmt(req, "RQF_MDS_HSM_PROGRESS")
     elif body.pb_opc == opcodes.OST_READ :
         show_request_fmt(req, "RQF_OST_BRW_READ")
     elif body.pb_opc == opcodes.OST_WRITE :
