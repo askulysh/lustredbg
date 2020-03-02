083bc69061c0c935a9dbdec6d842e7b0d4c391c5
Andriy Skulysh <askulysh@gmail.com>
2020-03-02 13:15:54 +0200

    lnet: add md lookup
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- lnet.py
+++ lnet.py
@@ -17,6 +17,55 @@ def nid2str(nid) :
     elif lnd == 13 :
         return ("%d@gni" % (nid & 0xffffffff))
 
+
+LNET_COOKIE_TYPE_BITS = 2
+LNET_COOKIE_MASK = (1 << LNET_COOKIE_TYPE_BITS) - 1
+
+LNET_LH_HASH_BITS = 12
+LNET_LH_HASH_SIZE = 1 << LNET_LH_HASH_BITS
+LNET_LH_HASH_MASK = LNET_LH_HASH_SIZE - 1
+
+the_lnet = readSymbol("the_lnet")
+LNET_CPT_NUMBER = the_lnet.ln_cpt_number
+LNET_CPT_BITS = the_lnet.ln_cpt_bits
+LNET_CPT_MASK = (1 << LNET_CPT_BITS) - 1
+
+def lnet_cpt_of_cookie(cookie) :
+    cpt = (cookie >> LNET_COOKIE_TYPE_BITS) & LNET_CPT_MASK
+
+    if cpt < LNET_CPT_NUMBER :
+        return cpt
+    else :
+        return  cpt % LNET_CPT_NUMBER
+
+def lnet_res_lh_lookup(rec, cookie) :
+
+    if (cookie & LNET_COOKIE_MASK) != rec.rec_type :
+        return 0
+
+    hash = cookie >> (LNET_COOKIE_TYPE_BITS + LNET_CPT_BITS)
+    handles = readSUListFromHead(rec.rec_lh_hash[hash & LNET_LH_HASH_MASK],
+                "lh_hash_chain", "struct lnet_libhandle")
+    for lh in handles :
+        if lh.lh_cookie == cookie :
+            return lh
+    return 0
+
+def lookup_md(cookie) :
+    cpt = lnet_cpt_of_cookie(cookie)
+    lh = lnet_res_lh_lookup(the_lnet.ln_md_containers[cpt], cookie)
+
+    if lh == 0 :
+        msg_container = the_lnet.ln_msg_containers[cpt]
+        active_msgs = readSUListFromHead(msg_container.msc_active,
+                "msg_activelist", "struct lnet_msg")
+
+        for msg in active_msgs :
+            if msg.msg_md.md_lh.lh_cookie == cookie:
+                return msg
+
+    return lh
+
 def print_nid(nid) :
     print(nid2str(nid))
 
@@ -25,7 +74,16 @@ if ( __name__ == '__main__'):
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-n","--nid", dest="nid", default = 0)
+    parser.add_argument("-m","--md", dest="md", default = 0)
+    parser.add_argument("-b", "--bulk", dest="bulk", default = 0)
     args = parser.parse_args()
     if args.nid != 0 :
         print_nid(int(args.nid, 16))
+    elif args.md != 0:
+        print(lookup_md(int(args.md, 16)))
+    elif args.bulk != 0 :
+        desc = readSU("struct ptlrpc_bulk_desc", int(args.bulk, 16))
+        for i in range(0, desc.bd_md_max_brw) :
+            print("cookie", desc.bd_mds[i].cookie)
+            print(lookup_md(desc.bd_mds[i].cookie))
 
