6e9cb31a913a00ce1627630c3b0e4beacb2eeec4
Andriy Skulysh <askulysh@gmail.com>
2020-04-20 13:28:02 +0300

    analyze: add --pid
    Signed-off-by: Andriy Skulysh <askulysh@gmail.com>

--- analyze.py
+++ analyze.py
@@ -1,6 +1,8 @@
 from pykdump.API import *
 from LinuxDump.BTstack import *
+import LinuxDump.KernLocks as kernlocks
 import ktime as ktime
+import obd as obd
 import ptlrpc as ptlrpc
 import ldlm_lock as ldlm
 import cl_io as cl_io
@@ -24,6 +26,13 @@ def show_client_pid(pid, prefix) :
         cl_io.print_dentry(dentry)
         cl_io.print_inode(prefix, dentry.d_inode)
 
+    addr = ptlrpc.search_stack_for_reg("RSI", stack, "ll_getattr")
+    if addr != 0 :
+        print()
+        dentry = readSU("struct dentry", addr)
+        cl_io.print_dentry(dentry)
+        cl_io.print_inode(prefix, dentry.d_inode)
+
     try :
         addr = ptlrpc.search_stack_for_reg("RDX", stack, "cl_lock_request")
     except:
@@ -32,6 +41,23 @@ def show_client_pid(pid, prefix) :
         print()
         cl = readSU("struct cl_lock", addr)
         cl_lock.print_cl_lock(cl, prefix)
+    else :
+        addr = ptlrpc.search_stack_for_reg("RSI", stack, "lov_lock_enqueue")
+        if addr != 0 :
+            print()
+            lock_slice = readSU("struct cl_lock_slice", addr)
+            cl_lock.print_cl_lock(lock_slice.cls_lock, prefix)
+
+    if addr != 0 :
+        print()
+        rqset = readSU("struct ptlrpc_request_set", addr)
+        ptlrpc.show_ptlrpc_set(rqset)
+
+    addr = ptlrpc.search_stack_for_reg("RDI", stack, "__mutex_lock_slowpath")
+    if addr != 0 :
+        print()
+        mutex = readSU("struct mutex", addr)
+        kernlocks.decode_mutex(mutex)
 
 def parse_client_eviction(stack) :
     addr = ptlrpc.search_stack_for_reg("RDI", stack, "ptlrpc_import_recovery_state_machine")
@@ -54,13 +80,7 @@ def parse_client_eviction(stack) :
     ptlrpc.imp_show_requests(imp)
     ptlrpc.imp_show_history(imp)
 
-if ( __name__ == '__main__'):
-    import argparse
-
-    parser =  argparse.ArgumentParser()
-    parser.add_argument("-l","--lock", dest="lock", default = 0)
-    args = parser.parse_args()
-
+def analyze_eviction() :
     btsl = exec_bt("bt")
     for bts in btsl :
         pid = bts.pid
@@ -74,3 +94,16 @@ if ( __name__ == '__main__'):
                 break
         if not parsed :
             print(bts)
+
+if ( __name__ == '__main__'):
+    import argparse
+
+    parser =  argparse.ArgumentParser()
+    parser.add_argument("-p","--pid", dest="pid", default = 0)
+    args = parser.parse_args()
+
+    if args.pid != 0 :
+        show_client_pid(int(args.pid), "")
+    else :
+        analyze_eviction()
+
